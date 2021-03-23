d242f9e3e887b335d00a925112cf22eaa7040b45
Avatar Azka <avatar.azka@gmail.com>
2021-03-23 14:55:50 +0700

    Removed debug printouts and main method

--- manager_node.py
+++ manager_node.py
@@ -16,57 +16,10 @@ def closeConnection(sc):
     sc.close()
 
 def sendState(sc, gameBoard):
-    print("Current board: ")
-    for row in gameBoard:
-        print(row)
-    print("Sending the board to the server...")
-
     data = pickle.dumps(gameBoard)
     sc.send(data)
 
 def receiveState(sc):
     outputValue = sc.recv(BUFFER_SIZE)
     nextBoard = pickle.loads(outputValue)
-    print("New board state: ")
-    for row in nextBoard:
-        print(row)
     return nextBoard
-
-def main():
-    """
-    GUI program
-    stay on while user has not quit
-    establish connection to server
-    every time player moves, send and recv
-    close connection when program ends
-    """
-    sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sc.connect((SERVER_IP, SERVER_PORT))
-
-    exampleBoard = [
-        [1, 2, 0],
-        [0, 2, 2],
-        [1, 0, 1]
-    ]
-
-    print("Tic-Tac-Toe Client Tester")
-    print("Current board: ")
-    for row in exampleBoard:
-        print(row)
-    print("Sending the board to the server...")
-
-    data = pickle.dumps(exampleBoard)
-    sc.send(data)
-
-    outputValue = sc.recv(BUFFER_SIZE)
-    nextBoard = pickle.loads(outputValue)
-    print("New board state: ")
-    for row in nextBoard:
-        print(row)
-
-    print("Closing connection...")
-    sc.close()
-
-
-if __name__ == "__main__":
-    main()
