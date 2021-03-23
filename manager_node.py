68bb99751fe5e12686cfe762554d11a6c31f2513
Avatar Azka <avatar.azka@gmail.com>
2021-03-23 18:07:13 +0700

    Refactored manager_node.py

--- manager_node.py
+++ manager_node.py
@@ -6,18 +6,22 @@ SERVER_IP = "127.0.0.1"
 SERVER_PORT = 4321
 BUFFER_SIZE = 1024
 
+
 def establishConnection():
     sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sc.connect((SERVER_IP, SERVER_PORT))
     return sc
 
+
 def closeConnection(sc):
     print("Closing connection...")
     sc.close()
 
+
 def sendState(sc, gameBoard):
     data = pickle.dumps(gameBoard)
     sc.send(data)
+
 
 def receiveState(sc):
     outputValue = sc.recv(BUFFER_SIZE)
